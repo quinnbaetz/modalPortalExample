9f3427cf71915a00fa9ea0629fb1bcb584b04376
Quinn Baetz <quinnbaetz@gmail.com>
2019-06-15 12:22:11 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css'
 import DeeplyNestedComponent from './DeeplyNestedComponent'
 function App() {
   return (
--- src/Modal.js
+++ src/Modal.js
@@ -1,12 +1,10 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 import Portal from './Portal'
 
 export default ({onCloseCallback}) => (
   <Portal>
     <div>
-      <p>A bunch of content</p>
+      <p>A bunch of modal content</p>
       <button onClick={onCloseCallback}>Close</button>
     </div>
   </Portal>
